988b4070eeef7e959df8b13687a4fbfbe252334b
pfreemanc <pfreemanc@gmail.com>
2020-10-15 14:37:35 -0400

    aos library added,jquery UI implementation, as well as slight animations.

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 
+function displayNav() {
+  $(".navLinks").toggle("slide", {
+    direction: "right",
+    duration: 500,
+  });
+  if(!$('.navLinks').hasClass('open')) {
+    console.log('open');
+    $('.navLinks').addClass('open');
+    $("#btnHamburger").html('<i class="fas fa-times"></i>');
+  } else {
+    $('.navLinks').removeClass('open');
+    $("#btnHamburger").html('<i class="fas fa-bars"></i>');
+    console.log('closed');
+  }
+}
 
+new ClipboardJS(".btnCopyEmail");
 
+$(function () {
+  console.log("ready");
+  AOS.init({
+    duration: 1500,
+  });
+  $('#btnHamburger').on('click', event => {
+    displayNav();
+  })
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+      $('#arrowDown').fadeOut();
+    }
+    else {
+      $('#arrowDown').fadeIn();
+    }
+  })
 
-$('h1').hide()
-
-$('h1').fadeIn(2000, function(){
-
-})
+  $('.btnCopyEmail').tooltip({
+    content: 'hey'
+  })
+});
